3d47c2d557e394b724295128f1675982ac486705
Isaac <isaac.w.donaldson@gmail.com>
2022-05-07 10:16:24 -0700

    added custom test framework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std] // Do not import the stdlib implicitly
 #![no_main] // Do not use normal rust entry chain (crt0 & main())
+// TEST
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -17,9 +21,43 @@ pub extern "C" fn _start() -> ! {
     // We will exit and not return so `!` is appropriate
     // Use C calling convention & default entry point `_start`
 
-    println!("The numbers are {} and {}", 42, 1.0 / 3.0);
-    println!("\n\n");
-    panic!("Some panic message");
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_asssertion() {
+    print!("trivial_assertion... ");
+    assert_eq!(true, true);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
