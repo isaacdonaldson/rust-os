ab1767c0434da134d78130709a8aa7c80fb8326c
Isaac <isaac.w.donaldson@gmail.com>
2022-05-07 15:55:04 -0700

    added support for integration tests, should_panic test example, and created lib.rs to have all test logic

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+pub mod serial;
+pub mod vga_buffer;
+
+/////////////////////////////////////////////
+// Test logic
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+// Implements the Testable trait for all types T
+// that implement the Fn() trait
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        // Uses the core library compiler type reflection to get the name of the type
+        // For functions the name is the function name
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+// Uses the Testable trait
+// Usable by other tests
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run(); // Call the run function on the Testable trait
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+// Usable by other tests
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {} // Still needed as compiler cannot tell we exit before here
+}
+
+/////////////////////////////////////////////
+// Entry Points
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+/////////////////////////////////////////////
+// Exits Qemu
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@
 #![no_main] // Do not use normal rust entry chain (crt0 & main())
 // TEST
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use blog_os::println;
 use core::panic::PanicInfo;
-
-mod serial;
-mod vga_buffer;
 
 // Need to define a panic handler
 // Conditional compilation for handler not in test
@@ -24,10 +22,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {} // Still needed as compiler cannot tell we exit before here
+    blog_os::test_panic_handler(info)
 }
 
 /////////////////////////////////////////////
@@ -42,61 +37,7 @@ pub extern "C" fn _start() -> ! {
     #[cfg(test)]
     test_main();
 
+    // panic!("at the disco");
+
     loop {}
-}
-
-/////////////////////////////////////////////
-// Test logic
-
-// Uses the Testable trait
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run(); // Call the run function on the Testable trait
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-// Implements the Testable trait for all types T
-// that implement the Fn() trait
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        // Uses the core library compiler type reflection to get the name of the type
-        // For functions the name is the function name
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[test_case]
-fn trivial_asssertion() {
-    assert_eq!(true, true);
-}
-
-/////////////////////////////////////////////
-// Exits Qemu
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
