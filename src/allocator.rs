71f5315ab8e32c15928b14ee454f8381b1bbdfef
Isaac <isaac.w.donaldson@gmail.com>
2022-08-11 11:26:30 -0700

    added bump allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,6 +1,5 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-use linked_list_allocator::LockedHeap;
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
@@ -8,8 +7,10 @@ use x86_64::{
     VirtAddr,
 };
 
+use bump::BumpAllocator;
+
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -50,4 +51,35 @@ pub fn init_heap(
     unsafe { ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE) }
 
     Ok(())
+}
+
+////////////////////////////////////////////////
+/// Allocators
+pub mod bump;
+
+/// Wrapper around spin::Mutex
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+/// Align the given address `addr` upwards to alignment `align`
+fn align_up(addr: usize, align: usize) -> usize {
+    let remainder = addr % align;
+    if remainder == 0 {
+        addr // already aligned
+    } else {
+        addr - remainder + align
+    }
 }
